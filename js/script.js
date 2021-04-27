43e5e6f55796330cea0402b465406cb898de57db
Ilya <ilyapavlushchik221@gmail.com>
2021-04-27 22:59:54 +0300

    Изменения формы
    -Добавил валидацию
    -Изменил div на form
    -Привязал текст к input через label

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const modalButton = document.querySelector('[data-modal-button]');
 const modal = document.querySelector('[data-modal]');
 const closemodalButton = document.querySelector('[data-modal-close]');
-console.log (modalButton);
+const submitmodalButton = document.querySelector('[data-modal-submit]');
 
 modalButton.addEventListener('click', function(){
     modal.classList.add('modal_active');
@@ -18,3 +18,28 @@ modal.addEventListener('click', function(){
 modal.querySelector('.modal__window').addEventListener('click', function(event){
     event.stopPropagation();
 });
+
+
+var form  = document.querySelector('.modal_content');
+var modalName = document.getElementById('modal_name');
+var modalTel = document.getElementById('modal_telephone');
+
+modalName.addEventListener("input", inputCheck, false);
+
+modalTel.addEventListener("input", inputCheck, false);
+form.addEventListener("submit", function (event) {
+  if (!modalName.validity.valid || modalName.value == "") {
+    modalName.classList.add('input_modal_error');
+    event.preventDefault();
+  }
+  if (!modalTel.validity.valid || modalTel.value == "") {
+    modalTel.classList.add('input_modal_error');
+    event.preventDefault();
+  }
+}, false);
+
+function inputCheck () {
+  if (this.validity.valid) {
+    this.classList.remove('input_modal_error');
+  }
+}
